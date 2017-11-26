61db64fb2a346d72d94c2c8abce2557e5fd1f83c
hamzahsn <amdouni.hamza@hotmail.fr>
2017-11-26 23:43:20 +0100

    adding the header page

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+<title>PDO OOP with Bootstrap</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> 
+</head>
+
+<body>
+
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+ 
+        <div class="navbar-header">
+           
+            <a class="navbar-brand" href="https://github.com/hamzahsn">hamzahsn</a>
+        </div>
+ 
+    </div>
+</div>
