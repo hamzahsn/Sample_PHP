c54dfc8516da130feb707fdb50cbe30942ce3030
hamzahsn <amdouni.hamza@hotmail.fr>
2017-11-26 23:45:09 +0100

    adding the footer page

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="alert alert-info">
+    	<a href="https://github.com/hamzahsn">hamzahsn</a>!
+	</div>
+</div>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
